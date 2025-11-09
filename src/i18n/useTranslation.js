0a83be153b6e6a3686ff9e7d4970db5849a6b8bf
nikoladach <nikoladacevski@gmail.com>
2025-11-09 17:36:59 +0100

    Delete src directory

--- src/i18n/useTranslation.js
+++
@@ -1,7 +0,0 @@
-import { useTranslation as useTranslationHook } from './translations';
-
-// Export the translation hook
-export const useTranslation = (language = 'mk') => {
-  return useTranslationHook(language);
-};
-
